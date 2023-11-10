59b37c052117cab56c3c3deb86f649ba59576283
danatdoxy <68253319+danatdoxy@users.noreply.github.com>
2023-11-09 21:44:23 -0500

    adding /summarize and /test

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import os
 
 app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def handle_slash_command():
+    # Parse the incoming JSON from the Slack command
+    data = request.form
+    command = data.get('command')
+    user_name = data.get('user_name')
+    text = data.get('text')
+
+    # Handle /summarize command
+    if command == '/summarize':
+        # This is where you would implement your summarization logic
+        response_text = f"Summarization requested by {user_name}. You asked to summarize: {text}"
+
+    # Handle /test command
+    elif command == '/test':
+        response_text = f"Test command received from {user_name}."
+
+    else:
+        response_text = f"The command {command} is not recognized."
+
+    # Prepare the response for Slack
+    response = {
+        "response_type": "in_channel",  # or "ephemeral" for a private message
+        "text": response_text,
+    }
+
+    return jsonify(response)
 
 
 @app.route('/')
